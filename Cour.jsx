a1eaf90de816aa38139a636812eb52803be53712
Manoj3759 <126229150+Manoj3759@users.noreply.github.com>
2023-03-09 19:15:28 +0530

    Add files via upload

---
+++ Cour.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+const Cour=(props)=>{
+    return(
+       <div className="main">
+        <div className="first">
+            <h1>{props.data[0].studentnamer}</h1>
+            <h2>{props.data[0].rollno}</h2>
+            <h2>{props.data[0].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[1].studentnamer}</h1>
+            <h2>{props.data[1].rollno}</h2>
+            <h2>{props.data[1].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[2].studentnamer}</h1>
+            <h2>{props.data[2].rollno}</h2>
+            <h2>{props.data[2].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[3].studentnamer}</h1>
+            <h2>{props.data[3].rollno}</h2>
+            <h2>{props.data[3].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[4].studentnamer}</h1>
+            <h2>{props.data[4].rollno}</h2>
+            <h2>{props.data[4].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[5].studentnamer}</h1>
+            <h2>{props.data[5].rollno}</h2>
+            <h2>{props.data[5].YOP}</h2>
+        </div> <div className="first">
+            <h1>{props.data[6].studentnamer}</h1>
+            <h2>{props.data[6].rollno}</h2>
+            <h2>{props.data[6].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[7].studentnamer}</h1>
+            <h2>{props.data[7].rollno}</h2>
+            <h2>{props.data[7].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[8].studentnamer}</h1>
+            <h2>{props.data[8].rollno}</h2>
+            <h2>{props.data[8].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[9].studentnamer}</h1>
+            <h2>{props.data[9].rollno}</h2>
+            <h2>{props.data[9].YOP}</h2>
+        </div> <div className="first">
+            <h1>{props.data[10].studentnamer}</h1>
+            <h2>{props.data[10].rollno}</h2>
+            <h2>{props.data[10].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[11].studentnamer}</h1>
+            <h2>{props.data[11].rollno}</h2>
+            <h2>{props.data[11].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[12].studentnamer}</h1>
+            <h2>{props.data[12].rollno}</h2>
+            <h2>{props.data[12].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[13].studentnamer}</h1>
+            <h2>{props.data[13].rollno}</h2>
+            <h2>{props.data[13].YOP}</h2>
+        </div> 
+        <div className="first">
+            <h1>{props.data[14].studentnamer}</h1>
+            <h2>{props.data[14].rollno}</h2>
+            <h2>{props.data[14].YOP}</h2>
+        </div>
+
+        <div className="first">
+            <h1>{props.data[15].studentnamer}</h1>
+            <h2>{props.data[15].rollno}</h2>
+            <h2>{props.data[15].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[16].studentnamer}</h1>
+            <h2>{props.data[16].rollno}</h2>
+            <h2>{props.data[16].YOP}</h2>
+        </div>
+         <div className="first">
+            <h1>{props.data[17].studentnamer}</h1>
+            <h2>{props.data[17].rollno}</h2>
+            <h2>{props.data[17].YOP}</h2>
+        </div> <div className="first">
+            <h1>{props.data[18].studentnamer}</h1>
+            <h2>{props.data[18].rollno}</h2>
+            <h2>{props.data[18].YOP}</h2>
+        </div>
+        <div className="first">
+            <h1>{props.data[19].studentnamer}</h1>
+            <h2>{props.data[19].rollno}</h2>
+            <h2>{props.data[19].YOP}</h2>
+        </div>
+        </div>
+    )
+}
+export default Cour;
