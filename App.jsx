9820b1b4072d938780d1ffc91991ae799a2dc2c1
Manoj3759 <126229150+Manoj3759@users.noreply.github.com>
2023-03-12 15:20:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Couser from './props/Couser';
+
+
+
+function App() {
+  return (
+    <>
+   <Couser   data={{
+    name:"JAVA full stack",
+    duration:"2 months",
+    available:true
+   }} />
+    </>
+  )
+}
+
+export default App
+
