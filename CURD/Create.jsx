8623e0ebea57a63da2e481f92a0313e4814f4e34
Manoj3759 <126229150+Manoj3759@users.noreply.github.com>
2023-03-30 20:42:56 +0530

    Add files via upload

---
+++ CURD/Create.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+import axiosInstance from '../helper/axiosInstance'
+
+const Create = () => {
+    let [course,setCourse]=useState('')
+    let [author,setAuthor]=useState('')
+
+    let handleClick=async(e)=>{
+        e.preventDefault()
+       try {
+        let payload={
+          course,
+          author
+        }
+        console.log(payload);
+        let data= await axiosInstance.post('/posts',payload)
+       } catch  {
+        console.log("Error");
+       }
+    }
+  return (
+    <>
+    <form action="">
+    <label htmlFor="course">CourseName: </label>
+    <input type="text" id='course' onChange={(e)=>{
+        setCourse(e.target.value)
+    }} /> <br />
+    <label htmlFor="author">AuthorName: </label>
+    <input type="text" id='author' onChange={(e)=>{
+        setAuthor(e.target.value)
+    }} /> <br />
+    <button onClick={handleClick}>Submit</button>
+    </form>
+    </>
+  )
+}
+
+export default Create
